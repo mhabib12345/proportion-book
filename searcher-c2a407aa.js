9216f9653870bb55e861148af16343e0b5d7121e
mhabib12345 <mhabib12345@users.noreply.github.com>
2026-01-03 13:05:13 +0000

    deploy: 0eda464a0d323b5f03f65cf82e0ed8e12e39a57d

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7ce460a6.js',
+                path_to_root + 'searchindex-b127f860.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
